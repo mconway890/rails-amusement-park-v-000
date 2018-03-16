74f75c29aa6f0071a58cd98521be6ca9f20220a3
EC2 Default User <ec2-user@ip-172-31-38-157.us-east-2.compute.internal>
2018-03-16 14:30:14 +0000

    Done.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -16,7 +16,7 @@ class Ride < ActiveRecord::Base
         user.nausea += attraction.nausea_rating
         user.happiness += attraction.happiness_rating
         user.save
-        "Thanks for riding the #{@ferriswheel.name}!"
+        "Thanks for riding the #{attraction.name}!"
       end
   end
 end
